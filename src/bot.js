6944090bf72395ef7a8b93fcaee57f9f1a67fa04
hkhsksdm <126839189+hkhsksdm@users.noreply.github.com>
2023-03-11 22:39:50 -0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -9,4 +9,49 @@ const client = new Client({
 
     ],
 });
-client.login("MTA4MzU0MjM4MjI4NjQ3OTQyMQ.G3Zoq_.T1-gjLlaUTc4k6_Toux0gckPPPkMDX4RAthHQQ");
+const token='MTA4MzU0MjM4MjI4NjQ3OTQyMQ.GQbjJa.Z1oA9CzsiYx1ApyZ7Q0Rto4K77OTkdsakALWRA';
+
+const PREFIX = 'Moew ';
+
+client.on('ready', (c) =>{
+    console.log(`${c.user.tag} is online.`);
+});
+
+//kitty words may hurt u but not me cuz i wrote it
+client.on('messageCreate',(message)=>{
+    
+    if(message.author.bot){
+        return;
+    }  
+
+    if(message.content.includes(PREFIX)){
+        let arr = message.content.substring(PREFIX.length).split(" ");
+        switch(arr[0]){
+            case 'help':
+                message.reply('sry cant cope');
+            break;
+
+            case 'work':
+                message.reply('hk dont have a job so im too');
+            break;
+        }
+    }
+
+    switch(message.content){
+        case 'cope':
+            message.reply('cope harder');
+        break;
+
+        case 'ok':
+            message.reply('its not questions but facts');
+        break;
+
+        case 'hello':
+            message.reply('shalom');
+        break;
+    }
+    if (message.content == 'Moew '|| message.content == 'Moew'){message.reply('what chu want')}
+});
+
+
+client.login(token);
